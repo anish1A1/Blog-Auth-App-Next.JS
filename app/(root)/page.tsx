6b377263d9a25a6b32be643186af16539b9f7590
anish1A1 <alloozpat87@gmail.com>
2024-12-03 14:24:45 +0545

    Installing Shadcn in the project and using it in the root folder of page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,21 @@
 
 export default function Home() {
   return (
-    <div className="heading">
-      Hello World
-    </div>
+    <>
+    <section className="pink_container">
+    
+    <h1 className="heading">
+      Pitch your startuP,
+      <br />
+      Connect with Entrepreneurs
+    </h1>
+
+{/* To override the default utility class we can use " ! " to override as !max-h-1 like this  */}
+    <p className="sub-heading !max-w-3xl">
+        Submit your startup idea and connect with like-minded entrepreneurs.
+    </p>
+
+    </section>
+    </>
   );
 }
